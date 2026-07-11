edff8056def2f3ad6646338abc74b1dc7454da6f
Johnathan <thark@rose-hulman.edu>
2026-07-11 18:14:51 -0400

    movie search with tmdb

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
-import BookSearch from './components/BookSearch'
-import MovieSearch from './components/MovieSearch'
-import './App.css'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import BookSearch from "./components/BookSearch";
+import MovieSearch from "./components/MovieSearch";
+import "./App.css";
 
-function Home(){
-  return <h1>Rec Page!</h1>
+function Home() {
+  return <h1>Rec Page!</h1>;
 }
 
 function App() {
-
   return (
     <BrowserRouter>
       <nav>
-        <Link to='/'>Home</Link> | {' '}
-        <Link to='/BookSearch'>Book Search</Link> | {' '}
-        <Link to='/MovieSearch'>Movie Search</Link> | {' '}
+        <Link to="/">Home</Link> | <Link to="/BookSearch">Book Search</Link> |{" "}
+        <Link to="/MovieSearch">Movie Search</Link> |{" "}
       </nav>
 
       <Routes>
-        <Route path='' element={<Home/>}></Route>
-        <Route path='/BookSearch' element={<BookSearch/>}></Route>
-        <Route path='/MovieSearch' element={<MovieSearch/>}></Route>
+        <Route path="" element={<Home />}></Route>
+        <Route path="/BookSearch" element={<BookSearch />}></Route>
+        <Route path="/MovieSearch" element={<MovieSearch />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
